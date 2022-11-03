5eff4a92300c9c1ba7da07532cc2ad5a2acd7d6b
Sasanka <kosuru.sasanka@gmail.com>
2022-11-03 01:30:16 -0700

    day3

---
+++ 141_linked_list_cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast_ptr = slow_ptr = head
+        
+        while fast_ptr and slow_ptr and fast_ptr.next:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+            
+            if slow_ptr == fast_ptr:
+                return True
+        return False
